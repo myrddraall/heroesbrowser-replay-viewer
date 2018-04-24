1273dace53b174fc248ab6269c73256f3a9aa9a5
derdelt <erd@conceptual.ca>
2018-04-24 16:23:46 -0400

    ga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { environment } from '../environments/environment';
 import { ReplayViewerComponent } from './replay-viewer/replay-viewer.component';
+
+
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+
 const appRoutes: Routes = [
 ];
 
@@ -10,7 +15,8 @@ const appRoutes: Routes = [
         RouterModule.forRoot(
             appRoutes,
             { enableTracing: !environment.production, useHash: true } // <-- debugging purposes only
-        )
+        ),
+        Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
     ],
     exports: [
         RouterModule
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {}
 }
