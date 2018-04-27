6ed56a8f5131789de54a3fe0f14baa82cdb6ded9
derdelt <erd@conceptual.ca>
2018-04-27 10:11:12 -0400

    minion death filters

--- src/app/replay-viewer/sections/globe-map/globe-map.component.ts
+++ src/app/replay-viewer/sections/globe-map/globe-map.component.ts
@@ -9,12 +9,20 @@ import {
 } from '@angular/core';
 
 import { ReplayViewerComponent } from '../../replay-viewer.component';
-import { Replay, ReplayMapAnalyser, IPoint } from '@heroesbrowser/heroprotocol';
+import { Replay, ReplayMapAnalyser, IPoint, IUnitLifeFilter } from '@heroesbrowser/heroprotocol';
 import { AbstractSectionComponent } from '../AbstractSection';
 import * as linq from 'linq';
 import { Subscription } from 'rxjs/Subscription';
 
 import { MapViewMode, MapIconCategory } from '../../maps';
+
+
+
+export interface IMinionFilter {
+  type?: string;
+  label: string;
+  filter: IUnitLifeFilter;
+}
 
 @Component({
   selector: 'globe-map',
@@ -23,6 +31,84 @@ import { MapViewMode, MapIconCategory } from '../../maps';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GlobeMapComponent extends AbstractSectionComponent {
+  public killedByFilter: IMinionFilter[] = [
+    {
+      label: 'Killed By Anything',
+      filter: undefined
+    },
+    {
+      label: 'On Team 1',
+      filter: {
+        isOnTeam: 0
+      }
+    },
+    {
+      label: 'On Team 2',
+      filter: {
+        isOnTeam: 1
+      }
+    },
+    {
+      label: 'Killed By Team 1',
+      filter: {
+        killedByTeams: 0
+      }
+    },
+    {
+      label: 'Killed By Team 2',
+      filter: {
+        killedByTeams: 1
+      }
+    },
+    {
+      label: 'Killed By Players',
+      filter: {
+        killedByPlayers: true
+      }
+    },
+    {
+      label: 'Killed By Team 1 Players',
+      filter: {
+        killedByTeams: 0,
+        killedByPlayers: true
+      }
+    },
+    {
+      label: 'Killed By Team 2 Players',
+      filter: {
+        killedByTeams: 1,
+        killedByPlayers: true
+      }
+    },
+    {
+      label: 'Killed By Minions',
+      filter: {
+        killedByMinions: true
+      }
+    },
+    {
+      label: 'Killed By Team 1 Minions',
+      filter: {
+        killedByTeams: 0,
+        killedByMinions: true
+      }
+    },
+    {
+      label: 'Killed By Team 2 Minions',
+      filter: {
+        killedByTeams: 1,
+        killedByMinions: true
+      }
+    },
+    {
+      label: 'Killed By Other',
+      filter: {
+        killedByMinions: false,
+        killedByPlayers: false
+      }
+    }
+
+  ];
 
   private replayMapAnalyser: ReplayMapAnalyser;
   public MapViewMode = MapViewMode;
@@ -34,6 +120,29 @@ export class GlobeMapComponent extends AbstractSectionComponent {
   ];
   public heatmap: IPoint[];
   public hasError = false;
+  private _currentFilter: IMinionFilter = this.killedByFilter[0];
+
+  public get currentFilter(): IMinionFilter { return this._currentFilter; }
+  public set currentFilter(value: IMinionFilter) {
+    if (this._currentFilter !== value) {
+      this._currentFilter = value;
+      this.updateHeatmap();
+    }
+
+  }
+  /*
+ <mat-option [value]="1">Anything</mat-option>
+          <mat-option [value]="2">Players</mat-option>
+          <mat-option [value]="2">Minions</mat-option>
+          <mat-option [value]="2">Team 1</mat-option>
+          <mat-option [value]="3">Team 2</mat-option>
+          <mat-option [value]="3">Team 1 Players</mat-option>
+          <mat-option [value]="3">Team 2 Players</mat-option>
+          <mat-option [value]="3">Team 1 Minions</mat-option>
+          <mat-option [value]="3">Team 2 Minions</mat-option>
+          <mat-option [value]="3">Other</mat-option>
+*/
+
 
   constructor(
     replayViewer: ReplayViewerComponent,
@@ -44,22 +153,22 @@ export class GlobeMapComponent extends AbstractSectionComponent {
   }
 
   protected async loadReplayView() {
-      this.replayMapAnalyser = new ReplayMapAnalyser(this.replay);
-      this.heatmap = await this.replayMapAnalyser.getMinionDeathHeatmap();
-      this.changeDetectorRef.markForCheck();
+    this.replayMapAnalyser = new ReplayMapAnalyser(this.replay);
+    await this.updateHeatmap();
   }
 
-  public cycleViewMode() {
-    this.viewMode++;
-    if (this.viewMode > 3) {
-      this.viewMode = 0;
-    }
+  private async updateHeatmap() {
+    this.heatmap = await this.replayMapAnalyser.getMinionDeathHeatmap(this.currentFilter.filter);
     this.changeDetectorRef.markForCheck();
   }
 
   public onMapviewError(err: Error) {
     this.hasError = true;
     this.setNotSupportedMessage(err.message);
+  }
+
+  public compareFilter(filter, selected): boolean {
+    return filter === selected;
   }
 
 }
