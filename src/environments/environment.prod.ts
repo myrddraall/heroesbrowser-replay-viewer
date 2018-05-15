325a8a858aa3abd2997e307b320af79f63c6d537
derdelt <erd@conceptual.ca>
2018-05-15 18:52:51 -0400

    build opt still breaks enums

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  appVerion: '0.9.0'
+  appVerion: '0.9.1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  appVerion: '0.9.0'
+  appVerion: '0.9.1'
 };
